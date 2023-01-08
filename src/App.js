b3da0b099daee2224d5500534c9131a4e942b0d7
Mayura Sandakalum <mssellapperuma@gmail.com>
2023-01-08 21:24:56 +0530

    Add vertical divider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Box } from '@mui/material';
 import Drawer from './components/Drawer';
+import { VerticalDividerSvg } from './components/svgs/DividerSvgs';
 
 function App() {
   return (
-    <>
+    <Box>
       <Drawer />
-    </>
+      <VerticalDividerSvg />
+    </Box>
   );
 }
 
